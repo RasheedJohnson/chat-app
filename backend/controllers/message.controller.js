3f03de3650b795174c4a770f2d8b5b1e37a55d88
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-07-07 01:32:11 +0200

    Add protectRoute middleware. update 'server.js', 'message.routes.js', 'message.controller.js', and create 'backend/middleware/protectRoute.js'

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,38 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
-    const { message } = await req.body;
-    const { id } = req.params;
-    const senderId = req.userId;
+    const { message } = await req.body; // Getting message from user as input
+    const { id: receiverId } = req.params; // Getting receiver id from params
+    const senderId = req.user._id; // Senderid coming from req.user, protectRoute
+
+    // FIND CONVERSATION BETWEEN USERS
+    let conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, receiverId],
+      },
+    });
+    // IF CONVERSATION DOESN'T EXIST, CREATE ONE
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+    // CREATE NEW MESSAGE
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+    // ADD MESSAGE TO MESSAGES ARRAY IN CONVERSATION
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+    // SAVE MESSAGE AND CONVERSATION TO DB IN PARALLEL
+    await Promise.all([conversation.save(), newMessage.save()]);
+    // FINALLY SEND MESSAGE JSON AS A RESPONSE
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sentMessage Controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -11,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // parse incoming requests with json payloads (from req.body)
+app.use(cookieParser()); // parse cookies
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
