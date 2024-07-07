28964f12693e66e1e680b02b1cfdd48e02f5a76e
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-07-08 00:43:52 +0200

    Complete 'get messages endpoint' to populate chat with messages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -38,3 +38,26 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+    // FIND CONVERSATION BETWEEN USERS
+    const conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, userToChatId],
+      },
+    }).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+    // IF CONVERSATION DOESN'T EXIST, DISPLAY EMPTY ARRAY OF MESSAGES
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+    // SAVE CONVERSATION MESSAGES AND RESPOND WITH CONVERSATION
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
