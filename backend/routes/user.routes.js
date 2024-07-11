648036bdfca24ede12f85ec7cb30893a5e99f3d9
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-07-11 21:42:36 +0200

    create and setup user.controller.js, include missing import within user.routes.js for getUsersForSidebar function

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute";
+import { getUsersForSidebar } from "../controllers/user.controller";
 
 const router = express.Router();
 
