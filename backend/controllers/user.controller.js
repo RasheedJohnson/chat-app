ffe84304f8cc20fd4e9c2458894328413e607a4d
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-07-11 21:56:09 +0200

    correct errors in importing functions without '.js' extension. complete testable 'Get Users for Sidebar Endpopint'. controller now also doesn't send passwords back as a response to all users as it did initially.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,14 @@
+import User from "../models/user.model.js";
+
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
+
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password"); // EVERY USER EXCEPT FOR LOGGED IN USER
+
+    res.status(200).json(filteredUsers);
   } catch (error) {
     console.error("Error in getUsersForSidebar function", error.message);
     res.status(500).json({ error: "Internal Server Error" });
