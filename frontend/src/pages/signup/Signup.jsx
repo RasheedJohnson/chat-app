3e76050d1f59c111ef2cc1399ce00c2eb62d52fd
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-07-23 23:17:11 +0200

    Create and prepare sign up page, and set up sidebar components

--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,3 +1,5 @@
+import GenderCheckbox from "./GenderCheckbox";
+
 const SignUp = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
@@ -35,12 +37,35 @@ const SignUp = () => {
             </label>
             <input
               type="password"
-              placeholder="Enter Password"
+              placeholder="Enter password"
               className="w-full input input-bordered h-10"
             />
           </div>
 
-          <div></div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm your password"
+              className="w-full input input-bordered h-10 mb-2"
+            />
+          </div>
+
+          {/* GENDER CHECKBOX */}
+          <GenderCheckbox />
+          <a
+            className="text-sm hover:underline hover:text-blue-500 mt-2 inline-block"
+            href="#"
+          >
+            Already have an account?
+          </a>
+          <div>
+            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
+              Sign Up
+            </button>
+          </div>
         </form>
       </div>
     </div>
@@ -48,3 +73,80 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
+// STARTER CODE FOR SIGNUP COMPONENT
+// import GenderCheckbox from "./GenderCheckbox";
+
+// const SignUp = () => {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+//       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-[8%]">
+//         <h1 className="text-3xl font-semibold text-center text-gray-300">
+//           Sign Up <span className="text-blue-300 font-light">Peer Chat </span>
+//         </h1>
+
+//         <form>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Full Name</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="John/Jane Doe"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Username</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="johnDoe/janeDoe"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Password</span>
+//             </label>
+//             <input
+//               type="password"
+//               placeholder="Enter password"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base label-text">Confirm Password</span>
+//             </label>
+//             <input
+//               type="password"
+//               placeholder="Confirm your password"
+//               className="w-full input input-bordered h-10 mb-2"
+//             />
+//           </div>
+
+//           {/* GENDER CHECKBOX */}
+//           <GenderCheckbox />
+//           <a
+//             className="text-sm hover:underline hover:text-blue-500 mt-2 inline-block"
+//             href="#"
+//           >
+//             Already have an account?
+//           </a>
+//           <div>
+//             <button className="btn btn-block btn-sm mt-2 border border-slate-700">
+//               Sign Up
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SignUp;
