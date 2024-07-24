570ab58ba2dba52beeb0fb57ba22b229856495c5
RasheedJohnson <rasheedjohnson@tutanota.com>
2024-07-24 13:24:08 +0200

    Set up UI for MessageContainer.jsx, Messages.jsx, and Message.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,3 +1,5 @@
+import Messages from "./Messages";
+
 function MessageContainer() {
   return (
     <div className="md:min-w-[450px] flex flex-col">
@@ -8,8 +10,8 @@ function MessageContainer() {
           <span className="text-gray-900 font-bold">John doe</span>
         </div>
 
-        {/* <Messages />
-				<MessageInput /> */}
+        <Messages />
+        {/* <MessageInput /> */}
       </>
     </div>
   );
